2358747851a83f217e2cc586ca74ec2a1eca0851
rv20823 <rv20823@gmail.com>
2024-09-19 09:45:05 +0000

    progress

--- blueprints/stories/models.py
+++ blueprints/stories/models.py
@@ -3,11 +3,21 @@ from blueprints.app import db
 class BlogPost(db.Model):
     __tablename__ = "blog_posts"
     pid = db.Column(db.Integer, primary_key=True)
+    # url_id = db.Column(db.TEXT, nullable=False)
     title = db.Column(db.VARCHAR(150), nullable=False)
     desc = db.Column(db.VARCHAR(200), nullable=False)
     body = db.Column(db.TEXT, nullable=False)
 
     def __repr__(self):
         return f"{self.title} {self.body} {self.desc}"
+
+class Comments(db.Model):
+    __tablename__ = "comments"
+    cid = db.Column(db.Integer, primary_key=True)
+    user_id = db.ForeignKey('blog_posts.pid', nullable=False)
+    body = db.Column(db.TEXT, nullable=False)
+
+    def __repr__(self):
+        return f"{self.name} {self.comment}"
 
 
