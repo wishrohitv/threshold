8ba50c3ac7fa01888b33d2e544f77a66943435d4
Rohit Vishwakarma <wishrohitv@proton.me>
2026-07-27 13:05:24 +0530

    Fix: Handle default value and type cast

--- settings.py
+++ settings.py
@@ -8,9 +8,9 @@ load_dotenv()
 class Settings:
     APP_NAME = os.environ.get("APP_NAME", "Threshold")
     APP_VERSION = os.environ.get("APP_VERSION", "1.0.1")
-    PORT = os.environ.get("PORT", 5000)
+    PORT = int(os.environ.get("PORT", "5000"))
     HOST = os.environ.get("HOST", "0.0.0.0")
-    DEBUG_MODE = os.environ.get("DEBUG_MODE", False)
+    DEBUG_MODE = os.environ.get("DEBUG_MODE", "False").lower()=="true"
     SECRET_KEY = os.environ.get("SECRET_KEY", token_hex(64))
 
     DATABASE_URI = os.environ.get("DATABASE_URI", "sqlite:///blog_data.db")
