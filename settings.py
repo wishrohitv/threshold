ebde438904c6403ac0b35faae0fa875426ef4697
Rohit Vishwakarma <wishrohitv@proton.me>
2026-07-27 13:31:00 +0530

    Ref: Optimize imports

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ class Settings:
     APP_VERSION = os.environ.get("APP_VERSION", "1.0.2")
     PORT = int(os.environ.get("PORT", "5000"))
     HOST = os.environ.get("HOST", "0.0.0.0")
-    DEBUG_MODE = os.environ.get("DEBUG_MODE", "False").lower()=="true"
+    DEBUG_MODE = os.environ.get("DEBUG_MODE", "False").lower() == "true"
     SECRET_KEY = os.environ.get("SECRET_KEY", token_hex(64))
 
     DATABASE_URI = os.environ.get("DATABASE_URI", "sqlite:///blog_data.db")
--- src/app.py
+++ src/app.py
@@ -1,16 +1,18 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from settings import Settings
-from werkzeug.middleware.proxy_fix import ProxyFix
 import os
+
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+from settings import Settings
 
 if Settings.DEBUG_MODE:
     # Allow insecure HTTP connections for local testing
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-from .utils.error_handler.page_not_found import return_404_page
 from .utils.context_processors.return_story_slug import return_story_slug
+from .utils.error_handler.page_not_found import return_404_page
 
 db = SQLAlchemy()
 
@@ -25,14 +27,14 @@ def create_app():
     app.config["SECRET_KEY"] = Settings.SECRET_KEY
 
     # Keep these to ensure the cookie survives the OAuth redirect
-    app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
-    app.config['SESSION_COOKIE_SECURE'] = True
+    app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
+    app.config["SESSION_COOKIE_SECURE"] = True
 
     db.init_app(app)
 
     # import models so SQLAlchemy can resolve relationships
-    from .blueprints.users.models import User, BlockedUser  # noqa: F401
-    from .blueprints.stories.models import Story, Comments, Like  # noqa: F401
+    from .blueprints.stories.models import Comments, Like, Story  # noqa: F401
+    from .blueprints.users.models import BlockedUser, User  # noqa: F401
 
     # create all tables
     with app.app_context():
@@ -42,11 +44,11 @@ def create_app():
     app.context_processor(return_story_slug)
 
     # import and register all blueprints
-    from .blueprints.index.routes import index_bp
-    from .blueprints.users.routes import users
-    from .blueprints.stories.routes import stories
     from .blueprints.about.routes import about
+    from .blueprints.index.routes import index_bp
     from .blueprints.search.routes import search_bp
+    from .blueprints.stories.routes import stories
+    from .blueprints.users.routes import users
 
     app.register_blueprint(index_bp, url_prefix="/")
 
